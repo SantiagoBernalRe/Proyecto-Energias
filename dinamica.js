49894ea62e9a624740b1a8a237677b6541954708
JFenixGG <121188150+JFenixGG@users.noreply.github.com>
2025-07-10 07:39:39 -0500

    Update dinamica.js

--- dinamica.js
+++ dinamica.js
@@ -1,7 +1,7 @@
 
 document.querySelectorAll('a.nav-link').forEach(link => {
   link.addEventListener('click', function(e) {
-    e.preventDefault();
+    
 
     const destino = document.querySelector(this.getAttribute('href'));
     if (destino) {
