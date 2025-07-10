83049149c21d0681c4f974ff558123b238177154
SantiagoBernalRe <santiagobernoldo@gmail.com>
2025-07-10 10:23:50 -0500

    Add files via upload

--- dinamica.js
+++ dinamica.js
@@ -1,7 +1,7 @@
 
 document.querySelectorAll('a.nav-link').forEach(link => {
   link.addEventListener('click', function(e) {
-    
+    e.preventDefault();
 
     const destino = document.querySelector(this.getAttribute('href'));
     if (destino) {
@@ -20,4 +20,33 @@ document.querySelectorAll('a.nav-link').forEach(link => {
     }
   });
 });
-function calcular{}
+const select = document.getElementById('opciones');
+const valorSeleccionado = select.value;
+const opcion = document.getElementById('seleccionaste');
+opcion.textContent(valorSeleccionado);
+const Ebase = document.getElementById(parseInt('acpm'));
+const TituloC = document.getElementById('tituloC');
+
+function TiempoEmisiones(){
+  if(opciones == 'eolica'){
+     TEbase=Ebase*15/60
+     TEolica=Ebase
+     EEbase=Ebase*800
+     EEolica=Ebase*5    
+  }
+  else if(opciones == 'solar'){
+    TEbase=Ebase*15/60
+    TSolar=Ebase
+    EEbase=Ebase*800
+    ESolar=Ebase*10
+  }
+  else if(opciones == 'biocombustible'){
+    TEbase=Ebase*15/60
+    TBiocombustible=Ebase*20/60
+    EEbase=Ebase*800
+    EBiocombustible=Ebase*70
+  }
+  else{
+    aviso = print("debes seleccionar unda de las energias")
+  }    
+}
